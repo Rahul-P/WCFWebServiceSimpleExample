d974bcaac60163137f9a28eb856605348050cf39
Rahul-P <rahulpawar@y7mail.com>
2015-03-01 21:14:44 +1100

    First Commit Setting up the Project.

---
+++ Tweet/Tweet.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Tweet
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [Serializable]
+    [DataContract]
+    public class Tweet
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string PostedBy { get; set; }
+
+        [DataMember]
+        public string Text { get; set; }
+    }
+}
